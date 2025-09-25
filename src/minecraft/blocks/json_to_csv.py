12cb5f5f7550bc0aa43dc1f5a4d941313fc7854e
jocooper <jocooper2018@gmail.com>
2025-09-25 08:46:13 +0200

    json to csv

---
+++ src/minecraft/blocks/json_to_csv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Run from project root
+
+import pandas as pd
+
+with open("./datasets/minecraft/blocks/blocklist_clean.json", encoding="utf-8") as inputfile:
+    df = pd.read_json(inputfile)
+
+df.to_csv("./datasets/minecraft/blocks/blocklist_clean.csv", encoding="utf-8", index=False)
